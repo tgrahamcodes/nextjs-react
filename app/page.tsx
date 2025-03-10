e077bada61558b4406023effd12cfbe907ae4062
Tom <iamtomgraham@gmail.com>
2025-03-09 20:50:40 -0700

    bcrypt

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+// Before (bcrypt)
+const bcrypt = require("bcrypt");
+// After (bcryptjs)
+const bcrypt = require("bcryptjs"
 
 export default function Page() {
   return (
