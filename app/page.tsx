b75de4390a45d9c1d5d739f994b9ac38329e7c71
Tom <iamtomgraham@gmail.com>
2025-03-09 21:06:14 -0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
-const bcrypt = require("bcryptjs");
 
 export default function Page() {
   return (
